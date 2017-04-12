faf71f1675b0c43ed281cc0810590c0711ecea52
kevin <kevin@motherapp.com>
2017-04-12 22:27:21 +0800

    Initial Commit

---
+++ QuickContact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickContact
+//
+//  Created by Kevin on 12/04/2017.
+//  Copyright © 2017 Mother App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
