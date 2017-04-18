9b83c3b3e1380b03342a5da027fcf6d5fc9bd7ad
kevin <kevin@motherapp.com>
2017-04-18 15:28:49 +0800

    contact

--- QuickContact/ContactListViewController.swift
+++ QuickContact/ContactListViewController.swift
@@ -9,11 +9,23 @@
 import UIKit
 import CoreData
 import Alamofire
+import Contacts
 
 class ContactListViewController: UIViewController {
     
     private var contactTableViewController: ContactTableViewController!
     lazy var coreDataStack = CoreDataStack()
+    var contactStore = CNContactStore()
+    var contacts = [CNContact]()
+
+    @IBAction func exportBtnClicked(_ sender: Any) {
+        requestForAccess { (accessGranted) -> Void in
+            if accessGranted {
+                print("kevin: get premiss success")
+                self.getAllContacts()
+            }
+        }
+    }
 
     @IBAction func refreshBtnClicked(_ sender: Any) {
         getJSON()
@@ -93,6 +105,88 @@ class ContactListViewController: UIViewController {
             let value = response.result.value as! [NSDictionary]
             self.clearContactDataList()
             self.setContactDataList(json: value)
+        }
+    }
+
+    func showMessage(message: String) {
+        let alertController = UIAlertController(title: "QuickContact", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        let dismissAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action) -> Void in
+        }
+        
+        alertController.addAction(dismissAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func requestForAccess(completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+        let authorizationStatus = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
+        
+        switch authorizationStatus {
+        case .authorized:
+            completionHandler(true)
+            
+        case .denied, .notDetermined:
+            self.contactStore.requestAccess(for: CNEntityType.contacts, completionHandler: { (access, accessError) -> Void in
+                if access {
+                    completionHandler(access)
+                } else {
+                    if authorizationStatus == CNAuthorizationStatus.denied {
+                        DispatchQueue.main.async(execute: { () -> Void in
+                            let message = "\(accessError!.localizedDescription)\n\nPlease allow the app to access your contacts through the Settings."
+                            self.showMessage(message: message)
+                        })
+                    }
+                }
+            })
+            
+        default:
+            completionHandler(false)
+        }
+    }
+    
+    func getAllContacts () {
+        
+        let keysToFetch = [
+            CNContactGivenNameKey,
+            CNContactFamilyNameKey,
+            CNContactPhoneNumbersKey
+        ]
+        let request = CNContactFetchRequest(keysToFetch: keysToFetch as [CNKeyDescriptor])
+        self.contacts = []
+        do {
+            try self.contactStore.enumerateContacts(with: request) { contact, stop in
+                self.contacts.append(contact)
+            }
+            
+            for contact in self.contacts {
+                print("contact:\(contact)")
+                
+                let firstName=String(format:"%@",contact.givenName)
+                print("first:\(firstName)")
+                
+                let lastName=String(format:"%@",contact.familyName)
+                print("last:\(lastName)")
+                
+                //get all phone numbers
+                // for ContctNumVar: CNLabeledValue in contact.phoneNumbers
+                // {
+                //     let MobNumVar  = (ContctNumVar.value ).value(forKey: "digits") as? String
+                //     print("ph no:\(MobNumVar!)")
+                // }
+                
+                // get one phone number
+                let MobNumVar = (contact.phoneNumbers[0].value ).value(forKey: "digits") as! String
+                print("mob no:\(MobNumVar)")
+                
+            }
+            
+            DispatchQueue.main.async(execute: {
+            })
+            
+            
+        } catch {
+            print(error)
         }
     }
 
